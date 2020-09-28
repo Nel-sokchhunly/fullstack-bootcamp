a2b3722ab4ff7b637ca78f752e591cc7e47aeee5
Nel-sokchhunly <60089527+Nel-sokchhunly@users.noreply.github.com>
2020-09-29 01:08:24 +0700

    fixed bug

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const posts = require('./routes/api/posts');
 app.use('/api/posts', posts); 
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/public/'));
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
 }
 
